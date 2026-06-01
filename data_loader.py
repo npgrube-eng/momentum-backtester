1a2bb4a1b6258c59705a150648295eed4511258b
npgrube-eng <npgrube@gmail.com>
2026-06-01 15:31:26 -0500

    data loader definition

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import yfinance as yf
+
 def load_data(ticker, start_date):
+    data = yf.download(ticker, start=start_date)
+    data = data.dropna()
+    return data
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import load_data 
+
 load_data()
-
 print(df.head())
-
 plot closing prices
