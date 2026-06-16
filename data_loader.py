25a4f4f617162467b0cb9b8f4577e556ee1926ad
npgrube-eng <npgrube@gmail.com>
2026-06-16 15:35:58 -0500

    - Added data loader module - Downloads historical equity data using yfinance - Cleans missing values - Plots closing prices with matplotlib

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import yfinance as yf
 
-def load_data(ticker, start_date):
-    data = yf.download(ticker, start=start_date)
+def load_data(ticker, start_date, end_date):
+    """
+    Downloads historical stock price data.
+
+    Parameters:
+        ticker (str): Stock ticker symbol
+        start_date (str): YYYY-MM-DD
+        end_date (str): YYYY-MM-DD
+
+    Returns:
+        pandas.DataFrame
+    """
+    data = yf.download(ticker, start=start_date, end=end_date)
     data = data.dropna()
     return data
 
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import load_data 
+import data_loader as dl
 
-load_data()
-print(df.head())
-plot closing prices
+
+ticker = "SPY"
+
+df = dl.load_data(
+    ticker,
+    start_date="2020-01-01",
+    end_date="2021-01-01"
+)
+
+print(df.info())
+print(df.describe())
+plt.plot(df['Close'])
+plt.title('Closing Prices')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
